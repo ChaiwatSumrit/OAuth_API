3141b117914679e714a6802339dc9778aa147fc6
P'Best <chaiwat.sunrit@gmail.com>
2019-08-08 10:09:44 +0700

    fix

--- router/index.js
+++ router/index.js
@@ -43,7 +43,7 @@ module.exports = function (app) {
     })
 
     app.post('/getAllProductByOwner', async (req, res) => {
-        console.log(`req.body : ${JSON.stringify(JSON.parse(body).owner)}`)
+        console.log(`req.body : ${JSON.stringify(JSON.parse(req).body.owner)}`)
         try {
             
 
