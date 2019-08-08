0a61ac0fdf6af503e7dcccf7e9a5de043bce2e06
P'Best <chaiwat.sunrit@gmail.com>
2019-08-08 09:48:21 +0700

    fix

--- router/index.js
+++ router/index.js
@@ -44,7 +44,7 @@ module.exports = function (app) {
 
     app.post('/getAllProductByOwner', async (req, res) => {
         try {
-            logger.debug(`req.body : ${req.body}`)
+            logger.debug(`req.body : ${JSON.stringify(req.body)}`)
 
             let userName = req.body.owner
             var result = (await new request().getAllProductByOwner(userName))
